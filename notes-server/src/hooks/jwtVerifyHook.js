24eacfa534fea3ba23519679d6317269c94c2a3d
Tasuku Sasaki <t.sasaki.revol@gmail.com>
2022-09-23 09:05:22 +0000

    jwt

--- notes-server/src/hooks/jwtVerifyHook.js
+++ notes-server/src/hooks/jwtVerifyHook.js
@@ -7,16 +7,10 @@ module.exports = async (request, reply)=>{
         if (request.url ==  '/authenticate'){
             return;
         }
-        var res = /Bearer /;
-        var authorizationHeader = request.headers.authorization;
-        if(res.test(authorizationHeader)){
-            var token = authorizationHeader.split(' ');
-            var decoded = jwt.verify(token[1], jwtSecret);
-            console.log( `OK: decoded.username=[${decoded.iss}]` )
-
-        } else{
-          reply.code(401).send('');
-        }
+        const res = /(?<=Bearer(\s)*)[a-zA-Z\d\-._~+/]+=*/;
+        const token = request.headers.authorization.match(res);
+        const decoded = jwt.verify(token[0], jwtSecret);
+        console.log( `OK: decoded.username=[${decoded.iss}]` )
       } catch(err) {
         console.log( `ERROR: err.message=[${err.message}]` );
         reply.code(401).send(err.message);
